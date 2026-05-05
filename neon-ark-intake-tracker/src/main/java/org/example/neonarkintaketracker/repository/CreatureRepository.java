071e782bdd11c58fc25f5630fa592b16e04dc708
mcamtat <mcamtat@yahoo.com>
2026-05-04 19:00:32 -0500

    Added boolean to check for duplicate names and ignoring case.

--- neon-ark-intake-tracker/src/main/java/org/example/neonarkintaketracker/repository/CreatureRepository.java
+++ neon-ark-intake-tracker/src/main/java/org/example/neonarkintaketracker/repository/CreatureRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 public interface CreatureRepository extends JpaRepository<Creature, Long> {
 
     List<Creature> findByStatusNot(String status);
+    boolean existsByNameIgnoreCase(String name);
 
     // No extra methods needed for basic "read" functionality
 
