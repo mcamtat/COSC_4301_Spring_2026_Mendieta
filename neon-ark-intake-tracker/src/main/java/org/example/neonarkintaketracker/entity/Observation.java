1ea5fef61e5943683ea49297906d9542981db551
mcamtat <mcamtat@yahoo.com>
2026-05-04 20:04:07 -0500

    Create observation table and class

---
+++ neon-ark-intake-tracker/src/main/java/org/example/neonarkintaketracker/entity/Observation.java
@@ -0,0 +1,30 @@
+package org.example.neonarkintaketracker.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "observations")
+public class Observation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String note;
+
+    @Column(name = "observed_at", nullable = false)
+    private LocalDateTime observedAt;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "creature_id", nullable = false)
+    @JsonIgnore
+    private Creature creature;
+}
